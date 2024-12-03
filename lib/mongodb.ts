b9fe23cb4b4f0098eaa0c50109f88a3e73518b39
paresiqbal <thecrowdel@gmail.com>
2024-12-03 08:57:36 +0700

    Enhance MongoDB client initialization with TypeScript types and global promise declaration

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -7,8 +7,13 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI;
 const options = {};
 
-let client;
+let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
+
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
 
 if (process.env.NODE_ENV === "development") {
   if (!global._mongoClientPromise) {
