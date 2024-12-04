3ab5b69923d85388691058bc1842907c056041fc
paresiqbal <thecrowdel@gmail.com>
2024-12-04 23:23:34 +0700

    Enhance homepage layout with image and buttons for user actions

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-2xl font-bold mb-4">SMA Negeri 1 Indonesia</h1>
-      <div className="text-center">
-        <h2>Document Management</h2>
+    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 flex">
+      <Image src="/assets/file.png" width={500} height={500} alt="file" />
+      <div>
+        <h1>SMA Negeri 1 Indonesia</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, et
-          molestias earum illum voluptate perspiciatis velit porro quod quas
-          voluptates!
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam,
+          aperiam?
         </p>
+        <div>
+          <Button>Register</Button>
+          <Button>Login</Button>
+        </div>
       </div>
     </div>
   );
