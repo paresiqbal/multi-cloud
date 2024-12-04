e87d36bd8e7a1e8fa3ca4d58c83d5aede707a3ac
paresiqbal <thecrowdel@gmail.com>
2024-12-04 23:57:51 +0700

    Add navigation links for Register and Login on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
+
+// ui
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex flex-col-reverse sm:flex-row items-center justify-center min-h-screen p-8 sm:p-20 gap-16">
       <Image src="/assets/file.png" width={500} height={500} alt="file" />
       <div className="text-center sm:text-left space-y-4">
-        <h1 className="text-3xl sm:text-5xl font-bold text-gray-800">
+        <h1 className="text-3xl sm:text-5xl font-bold">
           SMA Negeri 1 Indonesia
         </h1>
         <p className="text-base sm:text-lg leading-relaxed">
@@ -14,8 +17,12 @@ export default function Home() {
           aperiam?
         </p>
         <div className="flex flex-col sm:flex-row gap-4 mt-6">
-          <Button>Register</Button>
-          <Button>Login</Button>
+          <Link href="/auth/register">
+            <Button variant="outline">Register</Button>
+          </Link>
+          <Link href="/auth/login">
+            <Button>Login</Button>
+          </Link>
         </div>
       </div>
     </div>
