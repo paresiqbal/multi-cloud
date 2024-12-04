6ee4ef702c2b4c8eaaceeec084e8532a6c3f62ca
paresiqbal <thecrowdel@gmail.com>
2024-12-04 23:19:32 +0700

    Add Dashboard layout and page components for document management

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,16 @@
+import Navbar from "@/components/Navbar";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+export default function Dashboard() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-2xl font-bold mb-4">SMA Negeri 1 Indonesia</h1>
+      <div className="text-center">
+        <h2>Document Management</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, et
+          molestias earum illum voluptate perspiciatis velit porro quod quas
+          voluptates!
+        </p>
+      </div>
+    </div>
+  );
+}
