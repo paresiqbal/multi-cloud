9f59d47e985109cc1e60c5ed3ab8654c5919e266
paresiqbal <thecrowdel@gmail.com>
2024-12-04 23:32:42 +0700

    Refactor homepage layout for improved responsiveness and styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 flex">
+    <div className="flex flex-col-reverse sm:flex-row items-center justify-center min-h-screen p-8 sm:p-20 gap-16">
       <Image src="/assets/file.png" width={500} height={500} alt="file" />
-      <div>
-        <h1>SMA Negeri 1 Indonesia</h1>
-        <p>
+      <div className="text-center sm:text-left space-y-4">
+        <h1 className="text-3xl sm:text-5xl font-bold text-gray-800">
+          SMA Negeri 1 Indonesia
+        </h1>
+        <p className="text-base sm:text-lg leading-relaxed">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam,
           aperiam?
         </p>
-        <div>
+        <div className="flex flex-col sm:flex-row gap-4 mt-6">
           <Button>Register</Button>
           <Button>Login</Button>
         </div>
