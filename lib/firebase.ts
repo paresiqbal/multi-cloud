d79fcf54a1accf48b6f5902f858d931c08298f28
paresiqbal <thecrowdel@gmail.com>
2024-12-05 10:12:02 +0700

    Add Firebase Storage initialization to enable file storage capabilities

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
@@ -19,5 +20,6 @@ const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { app, auth, googleProvider, db };
+export { app, auth, googleProvider, db, storage };
