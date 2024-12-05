24aa2398050538ea7f58ae5a5a4182a8338aa354
paresiqbal <thecrowdel@gmail.com>
2024-12-05 10:44:51 +0700

    Add TaskStudent and SOP components for student task and SOP uploads

---
+++ app/dashboard/schools/task/page.tsx
@@ -0,0 +1,8 @@
+export default function TaskStudent() {
+  return (
+    <div>
+      <h1>Task Students Upload</h1>
+      <TaskStudent />
+    </div>
+  );
+}
---
+++ app/dashboard/students/sop/page.tsx
@@ -0,0 +1,10 @@
+import { SopUpload } from "@/components/SopUpload";
+
+export default function SOP() {
+  return (
+    <div>
+      <h1>Upload SOP</h1>
+      <SopUpload />
+    </div>
+  );
+}
