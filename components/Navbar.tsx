25826fb84bb6f8fb902e543448a67f111e871516
paresiqbal <thecrowdel@gmail.com>
2024-12-05 10:05:26 +0700

    Update Navbar component to include logo image and streamline user display

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useAuth } from "../hooks/useAuth";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Navbar() {
   const { user, logout } = useAuth();
@@ -10,18 +11,14 @@ export default function Navbar() {
   return (
     <nav className="p-4 border-b">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-xl font-bold">
-          Doc Management
+        <Link href="/dashboard" className="text-xl font-bold">
+          <Image src="/assets/file.png" alt="Logo" width={50} height={50} />
         </Link>
-        <div>
+        <div className="flex gap-4 justify-center items-center">
           {user ? (
             <>
-              <Link href="/dashboard" className="mr-4">
-                Dashboard
-              </Link>
-              <Button onClick={logout} variant="ghost">
-                Logout
-              </Button>
+              {user && <p>{user.email}</p>}
+              <Button onClick={logout}>Logout</Button>
             </>
           ) : (
             <>
