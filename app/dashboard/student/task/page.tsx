c9b46d8bc38308386a6ad13bfaaa4df6276f9043
paresiqbal <thecrowdel@gmail.com>
2024-12-05 20:01:30 +0700

    Refactor file upload functionality into TaskForm component; update StudentTask to use new form and improve user experience

--- app/dashboard/student/task/page.tsx
+++ app/dashboard/student/task/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { useState } from "react";
-import supabase from "@/lib/supabase";
 import { SupabaseFileList } from "@/components/FileList";
+import { TaskForm } from "@/components/TaskForm";
 import {
   Card,
   CardContent,
@@ -12,58 +11,14 @@ import {
 } from "@/components/ui/card";
 
 export default function StudentTask() {
-  const [file, setFile] = useState<File | null>(null);
-  const [uploading, setUploading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
-
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      setFile(event.target.files[0]);
-    }
-  };
-
-  const uploadFile = async () => {
-    if (!file) return;
-
-    setUploading(true);
-    setError(null);
-
-    try {
-      const filePath = `uploads/${file.name}`;
-      const { data, error } = await supabase.storage
-        .from("task")
-        .upload(filePath, file, {
-          contentType: file.type,
-          upsert: true,
-        });
-
-      if (error) throw error;
-
-      console.log("File uploaded:", data);
-    } catch (err: unknown) {
-      console.error("Upload error:", err);
-      if (err instanceof Error) {
-        setError(err.message);
-      } else {
-        setError("An unknown error occurred.");
-      }
-    } finally {
-      setUploading(false);
-    }
-  };
-
   return (
     <div className="container mx-auto mt-10 p-4 max-w-7xl">
       <h1 className="text-2xl font-bold mb-4">Tugas Siswa</h1>
       <div className="mb-8">
-        <h2 className="text-xl font-semibold mb-2">Upload SOP Sekolah</h2>
-        <input type="file" onChange={handleFileChange} />
-        <button onClick={uploadFile} disabled={uploading}>
-          {uploading ? "Uploading..." : "Upload"}
-        </button>
+        <h2 className="text-xl font-semibold mb-2">Upload Tugas Siswa</h2>
+        <TaskForm />
       </div>
 
-      {error && <p style={{ color: "red" }}>{error}</p>}
       <Card>
         <CardHeader>
           <CardTitle>Cari Tugas</CardTitle>
