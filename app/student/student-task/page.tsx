0d903e60b8395089d6e3c4b5a3268e515ee875df
paresiqbal <thecrowdel@gmail.com>
2024-12-09 15:46:10 +0700

    Enhance styling of Student Task Upload page for improved layout and responsiveness

--- app/student/student-task/page.tsx
+++ app/student/student-task/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function StudentTaskUpload() {
   return (
-    <div>
+    <div className="container mx-auto mt-10 p-4 max-w-7xl">
       <h1>Student Task Upload</h1>
       <TaskList />
     </div>
