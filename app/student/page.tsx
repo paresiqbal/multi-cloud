686b1caa640568056c7eafe13ccbeee1af22a278
paresiqbal <thecrowdel@gmail.com>
2024-12-10 13:26:53 +0700

    Add student task submission page and tabs component for account and password management

--- app/student/page.tsx
+++ app/student/page.tsx
@@ -1,16 +1,9 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
+import StudentTaskUpload from "./student-task/page";
 
 export default function StudentPage() {
   return (
     <div className="container mx-auto mt-10 p-4 max-w-7xl">
-      <h1>Student Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, aliquid?
-      </p>
-      <Link href="/student/student-task">
-        <Button>Cari Tugas</Button>
-      </Link>
+      <StudentTaskUpload />
     </div>
   );
 }
